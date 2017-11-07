4570c4480dd1d4a2d45a25d2094d859b75b062dd
Damián Rotta <damian_rott2@hotmail.com>
2017-11-07 14:36:11 -0300

    Add frees()
    Using "_" preffix for local variables
    Using size_t for local data variables

--- test/test_pdict.c
+++ test/test_pdict.c
@@ -28,6 +28,11 @@ void loadDict(void) {
     }
 }
 
+void free_keys_data(){
+    free(keys);
+    free(data);
+}
+
 void test_create_NewDictShouldBeEmpty(void) {
     TEST_ASSERT_TRUE(pdict_is_empty(D));
 }
@@ -45,53 +50,57 @@ void test_isEmpty_ShouldReturnTrueOnAnEmptyDict(void) {
 }
 
 void test_size_SizeOfNewDictWithOneItemShouldBeOne(void) {
-    char *key = malloc(sizeof(char[1][KEYS_LEN]));
-    data = calloc(1, sizeof(size_t));
-    data[0] = 0;
-    snprintf(key, KEYS_LEN, "%s", "0_key");
-    pdict_put(D, key, &data[0]);
+    char *_key = malloc(KEYS_LEN);
+    size_t _data = 0;
+    snprintf(_key, KEYS_LEN, "%s", "0_key");
+    pdict_put(D, keys[0], &_data);
     TEST_ASSERT_EQUAL_UINT(pdict_size(D), 1);
+    free(_key);
 }
 
 void test_size_ShouldBeEqualToTheNumberOfAddedElements(void) {
     loadDict();
     TEST_ASSERT_EQUAL_UINT(pdict_size(D), DICT_DATA_LEN);
+    free_keys_data();
 }
 
 void test_size_ShouldBeZeroForACleanDict(void) {
     loadDict();
     pdict_clean(D);
     TEST_ASSERT_EQUAL_UINT(pdict_size(D), 0);
+    free_keys_data();
 }
 
 void test_size_ShouldNotBeZeroAfterAddingElements(void) {
     loadDict();
     TEST_ASSERT_TRUE(pdict_size(D) != 0);
+    free_keys_data();
 }
 
 void test_add_ShouldAddANewElement(void) {
-    char *key = malloc(sizeof(char[1][KEYS_LEN]));
-    data = calloc(1, sizeof(size_t));
-    snprintf(key, KEYS_LEN, "%s", "99_key");
-    data[0] = 99;
+    char *_key = malloc(KEYS_LEN);
+    size_t _data = 99;
+    snprintf(_key, KEYS_LEN, "%s", "99_key");
     TEST_ASSERT_TRUE(pdict_is_empty(D));
-    pdict_put(D, key, &data[0]);
+    pdict_put(D, _key, &_data);
     TEST_ASSERT_EQUAL_UINT(1, pdict_size(D));
+    free(_key);
 }
 
 void test_append_ShouldAddAnElementToAnEmptyDict(void) {
-    char* key = malloc(sizeof(char[1][KEYS_LEN]));
-    data = calloc(1, sizeof(size_t));
-    snprintf(key, KEYS_LEN, "%s", "1_key");
-    data[0] = 1;
+    char* _key = malloc(sizeof(char[1][KEYS_LEN]));
+    snprintf(_key, KEYS_LEN, "%s", "1_key");
+    size_t _data = 1;
     TEST_ASSERT_TRUE(pdict_is_empty(D));
-    pdict_put(D, key, &data[0]);
+    pdict_put(D, _key, _data);
     TEST_ASSERT_FALSE(pdict_is_empty(D));
+    free(_key);
 }
 
 void test_isEmpty_ShouldReturnFalseOnALoadedDict(void) {
     loadDict();
     TEST_ASSERT_FALSE(pdict_is_empty(D));
+    free_keys_data();
 }
 
 void test_get_ShouldGetAllValuesFromTheDict(void) {
@@ -101,6 +110,7 @@ void test_get_ShouldGetAllValuesFromTheDict(void) {
         TEST_ASSERT_NOT_NULL(element);
         TEST_ASSERT_EQUAL_UINT(element, &data[i]);
     }
+    free_keys_data();
 }
 
 int main(void) {
@@ -127,9 +137,7 @@ int main(void) {
 }
 
 /* Pending
-
 void_test_get_ShouldGetAllKeysValuesFromTheDict(){};
-
 test_plist based:
 void test_remove_ShouldRemoveAnElementFromDict(void) {}
 void test_remove_ShouldRemoveFirstElementFromDict(void) {}
